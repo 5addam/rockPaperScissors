c3f5bbd60ee2db82c830e2fbc02f181dc13d54e0
5addam <saddam.mughal60@gmail.com>
2020-08-31 01:11:08 +0500

    GUI Updatedgit add -A

--- rps.js
+++ rps.js
@@ -1,8 +1,12 @@
 const objects = ['rock','paper','scissors'];
 let btn = document.getElementById('rock');
-let message = document.getElementById('message');
+// let message = document.getElementById('message');
 let player = document.getElementById('score-player');
 let computer = document.getElementById('score-computer');
+let buttons = document.querySelectorAll('button');
+const leftHand = document.getElementById('hand-img-user');
+const rightHand = document.getElementById('hand-img-computer');
+const result = document.getElementById('result');
 let pScore = 0;
 let cScore = 0; 
 
@@ -13,30 +17,59 @@ function computerPlay(){
 
 function playRound(playerSelection, computerSelection){
   if (playerSelection === computerSelection){
-    return `You choose ${playerSelection} \& Computer choose ${computerSelection} 
-    \nIt was a Tie!! \n You both choose ${playerSelection}`;
+    return `It's a Tie!!`;
   }
   else if(playerSelection === 'rock' && computerSelection === 'scissors'){
     pScore++;
-    return `You choose ${playerSelection} \& Computer choose ${computerSelection} \n You WON!! \n Rock beats Scissors`;
+    return `You WON!!`;
   }
   else if(playerSelection === 'paper' && computerSelection === 'rock'){
     pScore++;
-    return `You choose ${playerSelection} \& Computer choose ${computerSelection}\nYou WON!! \n Paper beats Rock`;
+    return `You WON!!`;
   }
   else if(playerSelection === 'scissors' && computerSelection === 'paper'){
     pScore++;
-    return `You choose ${playerSelection} \& Computer choose ${computerSelection} \n You WON!! \n Scissors beats Paper`;
+    return `You WON!!`;
 }
 else{
   cScore++;
-  return `You choose ${playerSelection} \& Computer choose ${computerSelection} \n You LOSE!! \n ${computerSelection} beats ${playerSelection}`;
+  return `You LOSE!!`;
 }
 }
 
-function game(userInput){
-  let output = playRound(userInput, computerPlay());
-  message.innerHTML = output;
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function game(userInput){
+  await sleep(3000);
+  let computerInput = computerPlay();
+  let output = playRound(userInput, computerInput);
+  changeImage(userInput, computerInput);
+  await sleep(300);
+  result.textContent = output;
+  // result.style.opacity = 0;
+  result.setAttribute('style','opacity:1;');
   player.textContent = pScore.toString();
   computer.textContent = cScore.toString();
+  
+  leftHand.classList.remove('left-anim');
+  rightHand.classList.remove('right-anim');
 }
+
+function changeImage(userInput, computerInput){
+  rightHand.setAttribute('src', `right${computerInput}.png`);
+  leftHand.setAttribute('src', `left${userInput}.png`);
+
+}
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () =>{
+    result.setAttribute('style','opacity:0;');
+    leftHand.classList.toggle('left-anim');
+    rightHand.classList.toggle('right-anim');
+    changeImage('rock', 'rock');
+    game(button.value);
+  });
+
+});
